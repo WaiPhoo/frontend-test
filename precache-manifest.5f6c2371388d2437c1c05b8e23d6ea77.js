031b6d648ce4310045c5d15e28e49b0697a07378
WaiPhoo <waiphookhaing@gmail.com>
2018-12-26 16:00:21 +0630

    Updates

---
+++ precache-manifest.5f6c2371388d2437c1c05b8e23d6ea77.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ece25337b6897b94c96f",
+    "url": "/frontend-test/static/js/runtime~main.ece25337.js"
+  },
+  {
+    "revision": "aada9293907e7e529512",
+    "url": "/frontend-test/static/js/main.aada9293.chunk.js"
+  },
+  {
+    "revision": "00c31166f8634460c282",
+    "url": "/frontend-test/static/js/1.00c31166.chunk.js"
+  },
+  {
+    "revision": "aada9293907e7e529512",
+    "url": "/frontend-test/static/css/main.a890dabe.chunk.css"
+  },
+  {
+    "revision": "55a29a891ccd82346d4fa643addad0dd",
+    "url": "/frontend-test/index.html"
+  }
+];
